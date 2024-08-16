d848d7079307e7c0ae56f3163c1e717775ac8d42
Annpurna Gupta <65756912+annpurna04@users.noreply.github.com>
2024-08-16 23:47:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*========== toggle icon navbar=========*/
+/========== toggle icon navbar=========/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 menuIcon.onclick =  () => {
@@ -6,7 +6,7 @@ menuIcon.onclick =  () => {
     navbar.classList.toggle('active');
 
 }
-/*========== scroll section=========*/
+/========== scroll section=========/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -25,15 +25,15 @@ window.onscroll = () => {
         };
         
     });
-    /*========== sticky navbar=========*/
+    /========== sticky navbar=========/
     let header=document.querySelector('header');
 
-    header.classList.toggle('sticky',window.scrollY < 100);
-        /*==========remove toggle icon and navbar when click navbar link =========*/
+    header.classList.toggle('sticky',window.scrollY > 100);
+        /==========remove toggle icon and navbar when click navbar link =========/
 menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
 };
-    /*==========scroll reveal=========*/
+    /==========scroll reveal=========/
 
 ScrollReveal({ 
     //reset: true,
@@ -45,11 +45,33 @@ ScrollReveal().reveal('.home-content,.heading', { origin:'top' });
 ScrollReveal().reveal('.home-img,.portfoli-box,.heading', { origin:'bottom' });
 ScrollReveal().reveal('.home-content h1,.about-img', { origin:'left' });
 ScrollReveal().reveal('.home-content p,.about-content,.card', { origin:'right' });
-    /*==========typed js=========*/
-    const typed=new typed('.multiple-text',{
+    /==========typed js=========/
+    const typed=new Typed('.multiple-text',{
         strings: ['Frontend Developer','Programmer'],
         typeSpeed:100,
         backSpeed:100,
         backDelay:1000,
         loop:true
     });
+
+window.onscroll=()=>{
+    sections.forEach(sec => {
+        let top=window.scrollY;
+        let offset=sec.offsetTop-150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id');
+
+        if(top>=offset && tp<offset+height){
+            navLinks.forEach(links=> {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' +id + ']').classList.add('active');
+
+            });
+        };
+        
+    });
+    /========== sticky navbar=========/
+    let header=document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY>100);
+};
